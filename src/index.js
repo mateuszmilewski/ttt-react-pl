be90823f3005c5132bd8f780812df0f8987a217e
forrest <forrest@MacBook-Air-forrest.local>
2020-05-08 13:20:32 +0200

    status = 'calc winner added'

--- src/index.js
+++ src/index.js
@@ -46,51 +46,85 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        
+        const winner = calculateWinner(this.state.arr);
+        let status;
+
+        if(winner) {
+            status = "Wygrywa: " + winner;
+        } else {
+            status = 'Next player: ' + '' + (this.state.isNext ? 'X' : 'O');    
+        }
+        
 
         return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
         );
     }
 }
   
-  class Game extends React.Component {
+class Game extends React.Component {
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+        return (
+            <div className="game">
+                <div className="game-board">
+                <Board />
+                </div>
+                <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
     }
-  }
+}
   
-  // ========================================
+// ========================================
   
-  ReactDOM.render(
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
+);
+
+
+function calculateWinner(sq) {
+
+
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (sq[a] && sq[a] === sq[b] && sq[a] === sq[c]) {
+        return sq[a];
+      }
+    }
+    return null;
+}
   
