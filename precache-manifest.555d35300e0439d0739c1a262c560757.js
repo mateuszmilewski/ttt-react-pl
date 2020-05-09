3d26396fdbabacff421fb87a9cdede517e8db4c1
forrest <forrest@MacBook-Air-forrest.local>
2020-05-09 10:49:45 +0200

    Updates

---
+++ precache-manifest.555d35300e0439d0739c1a262c560757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afa4d9743b5cd269c2d4a79dffcd490",
+    "url": "/ttt2/index.html"
+  },
+  {
+    "revision": "9a45e465e6faf752087f",
+    "url": "/ttt2/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "9773d4e58f0817a725dd",
+    "url": "/ttt2/static/js/2.3a4041ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ttt2/static/js/2.3a4041ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a45e465e6faf752087f",
+    "url": "/ttt2/static/js/main.ec4f3a01.chunk.js"
+  },
+  {
+    "revision": "0703e1aa3cf31a3f8ae0",
+    "url": "/ttt2/static/js/runtime-main.d14371dd.js"
+  }
+]);
